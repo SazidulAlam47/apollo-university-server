1957aa37d18c1c614a4a112542b5644cb036c6a5
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-02 23:45:45 +0600

    get and post method working

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,48 @@
+import { Student } from './student.interface';
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+
+const createStudent = async (req: Request, res: Response) => {
+    try {
+        const { student } = req.body;
+
+        const result = await StudentServices.createStudentIntoDB(student);
+
+        res.status(201).json(
+            {
+                success: true,
+                message: 'Student created successfully',
+                data: result
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong'
+        });
+    }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+    try {
+        const result = await StudentServices.getAllStudentsFromDB();
+        res.status(200).json({
+            success: true,
+            message: 'All students fetched successfully',
+            data: result
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong'
+        });
+    }
+}
+
+export const StudentControllers = {
+    createStudent,
+    getAllStudents
+};
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export interface UserName {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+}
+
+export interface Guardian {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContact: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContact: string;
+}
+
+export interface LocalGuardian {
+    name: string;
+    occupation: string;
+    contactNo: string;
+    address: string;
+}
+
+export interface Student {
+    id: string;
+    name: UserName;
+    gender: "Male" | "Female";
+    dateOfBirth: string;
+    email: string;
+    contactNumber: string;
+    emergencyContact: string;
+    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: Guardian;
+    localGuardian: LocalGuardian;
+    profileImg?: string;
+    isActive: "Active" | "Blocked";
+}
