2bcbe45ddb27b0b6bca4f6dfeb6fbbcbcfb7579c
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-02 12:50:04 +0600

    some changes before eslint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-import express, { Request, Response } from 'express';
-const app = express();
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
