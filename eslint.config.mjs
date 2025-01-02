d6a9bfa422c012e0d8812b99f42b94bff5b0241c
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-02 14:01:09 +0600

    before prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.browser, process: "readonly" } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
       "no-unused-vars": "error",
       "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+      "no-undef": "error",
     },
   },
   {
-    ignores: [".node_modules/*"]
+    ignores: [".node_modules/*"],
   },
-
 ];
