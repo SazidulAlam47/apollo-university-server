8352658bf2fd72029d2c461e7463cd675406a99a
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-27 23:04:16 +0600

    more refactor

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 const app: Application = express();
 
 // parsers
@@ -11,12 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+const test = (req: Request, res: Response) => {
+    res.send("PH University Server is Running...");
+};
+
+app.get("/", test);
 
 // global error handler
 app.use(globalErrorHandler);
