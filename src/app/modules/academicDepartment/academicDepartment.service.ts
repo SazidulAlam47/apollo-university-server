3ee75262947a09a65e6c22dc42c652ed56efd406
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-31 15:26:57 +0600

    populate

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -7,12 +7,13 @@ const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
 };
 
 const getAllAcademicDepartmentFromDB = async () => {
-    const result = await AcademicDepartment.find();
+    const result = await AcademicDepartment.find().populate('academicFaculty');
     return result;
 };
 
 const getAcademicDepartmentByIdFromDB = async (id: string) => {
-    const result = await AcademicDepartment.findById(id);
+    const result =
+        await AcademicDepartment.findById(id).populate('academicFaculty');
     return result;
 };
 
