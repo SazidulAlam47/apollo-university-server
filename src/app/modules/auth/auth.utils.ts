5384d2e9590aedd7e0eac8fc9c983b1e45c11209
Sazidul Alam <sazidulalam20@gmail.com>
2025-03-16 15:26:00 +0600

    token expire error handle

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
 import jwt, { JwtPayload, SignOptions } from 'jsonwebtoken';
 import ms from 'ms';
+import AppError from '../../errors/AppError';
+import status from 'http-status';
 
 export const createToken = (
     jwtPayload: {
@@ -13,5 +16,11 @@ export const createToken = (
     return jwt.sign(jwtPayload, secret, options);
 };
 
-export const verifyToken = (token: string, secret: string) =>
-    jwt.verify(token, secret) as JwtPayload;
+export const verifyToken = (token: string, secret: string) => {
+    try {
+        return jwt.verify(token, secret) as JwtPayload;
+    } catch (err) {
+        console.log(err);
+        throw new AppError(status.UNAUTHORIZED, 'You are not authorized');
+    }
+};
