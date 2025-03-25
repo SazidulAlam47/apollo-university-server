b35b946a6b5e06f5c24c836977a2c1fd373b941f
Sazidul Alam <sazidulalam20@gmail.com>
2025-03-25 18:54:54 +0600

    password validation

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,5 +1,27 @@
 import { z } from 'zod';
 
+const newPasswordSchema = z
+    .string({ required_error: 'Please enter your Password' })
+    .min(6, { message: 'Password must be at least 6 characters long' })
+    .refine((password) => /[a-zA-Z]/.test(password), {
+        message: 'Password must contain at least one letter',
+    })
+    .refine((password) => /[a-z]/.test(password), {
+        message: 'Password must contain at least one lowercase letter',
+    })
+    .refine((password) => /[A-Z]/.test(password), {
+        message: 'Password must contain at least one uppercase letter',
+    })
+    .refine((password) => /[0-9]/.test(password), {
+        message: 'Password must contain at least one number',
+    })
+    .refine(
+        (password) => /[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]/.test(password),
+        {
+            message: 'Password must contain at least one special character',
+        },
+    );
+
 const loginValidationSchema = z.object({
     body: z.object({
         id: z.string({ required_error: 'ID is required' }),
@@ -10,7 +32,7 @@ const loginValidationSchema = z.object({
 const changePasswordValidationSchema = z.object({
     body: z.object({
         oldPassword: z.string({ required_error: 'Old password is required' }),
-        newPassword: z.string({ required_error: 'Password is required' }),
+        newPassword: newPasswordSchema,
     }),
 });
 
@@ -29,7 +51,7 @@ const forgetPasswordValidationSchema = z.object({
 const resetPasswordValidationSchema = z.object({
     body: z.object({
         id: z.string({ required_error: 'ID is required' }),
-        password: z.string({ required_error: 'Password is required' }),
+        password: newPasswordSchema,
     }),
 });
 
