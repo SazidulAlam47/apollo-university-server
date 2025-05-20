1a9c1c290ab5d8104eb2f7981954a6ce9bc94677
Sazidul Alam <sazidulalam20@gmail.com>
2025-05-20 20:28:51 +0600

    fix: update Zod error handling status code to UNPROCESSABLE_ENTITY

--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -3,7 +3,7 @@ import { ZodError, ZodIssue } from 'zod';
 import { TErrorSources, TGenericErrorResponse } from '../interface/error';
 
 const handleZodError = (err: ZodError): TGenericErrorResponse => {
-    const statusCode = status.BAD_REQUEST;
+    const statusCode = status.UNPROCESSABLE_ENTITY;
     const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => ({
         path: issue.path[issue.path.length - 1],
         message: issue.message,
